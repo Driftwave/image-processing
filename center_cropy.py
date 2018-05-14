45e51d068b85d20fa44e5560850a12013c29e0f1
Benjamin Snyder <ben@driftwave.ai>
2018-05-14 15:55:28 -0400

    first commit

---
+++ center_cropy.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+center_crop.py - Performs downscaling and center-crop of images in directory
+
+Usage:
+  center_crop.py <image_dir> <resolution>
+"""
+import glob
+import os
+import sys
+
+from docopt import docopt
+from PIL import Image, ImageOpts
+
+
+def parse_resolution_arg(resolution):
+    try:
+        if 'x' not in resolution:
+            size = int(resolution)
+            return size, size
+        else:
+            width, height = (int(s) for s in resolution.split('x'))
+            return width, height
+    except Exception:
+        print("couldn't parse resolution.")
+
+
+def main():
+    args = docopt(__doc__, help=True)
+    image_dir = args['<image_dir>']
+    resolution = args['<resolution>']
+    image_paths = glob.glob(image_dir + '/*.jpg')
+    if len(image_path) == 0:
+        print('no images (or image directory) found.', file=sys.stderr)
+        sys.exit(1)
+
+        size = parse_resolution_arg(resolution)
+        resolution = width + 'x' + height
+        output_dir = os.path.join(image_dir, resolution)
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+
+        for img_path in image_paths:
+            output_path = os.path.join(output_dir, os.path.basename(img_path))
+            if os.path.exists(output_path):
+                continue
+            img = Image.open(img_path)
+            img = ImageOps.fit(img, size, method=Image.BICUBIC)
+            img.save(output_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ compute_image_vectors.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+compute_image_vectors.py - computes image vectors of jpegs
+
+Usage:
+  compute_image_vectors.py <image_dir> <output_prefix>
+"""
+import glob
+import os
+
+from docopt import docopt
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+
+MODULE_NAME = 'nasnet_large'
+MODULE_URL = 'https://tfhub.dev/google/imagenet/' + MODULE_NAME + '/feature_vector/1'
+
+
+def create_module_graph(module_spec):
+    height, width = hub.get_expected_image_size(module_spec)
+    with tf.Graph().as_default() as graph:
+        resized_image_tensor = tf.placeholder(tf.float32, [None, height, width, 3])
+        m = hub.Module(module_spec)
+        bottleneck_tensor = m()
+    return graph, bottleneck_tensor, resized_input_tensor
+
+
+def run_bottleneck_on_image(sess, image_data, image_data_tensor, decoded_image_tensor, resized_image_tensor,
+                            bottleneck_tensor):
+    resized_input_values = sess.run(decoded_image_tensor, {image_data_tensor: image_data})
+    bottleneck_values = sess.run(bottleneck_tensor, {resized_input_tensor: resized_input_values})
+    bottleneck_values = np.squeeze(bottleneck_values)
+    return bottleneck_values
+
+
+def add_jpeg_decoding(module_spec):
+    height, width = hub.get_expected_image_size(module_spec)
+    jpeg_data = tf.placeholder(tf.string, name='DecodeJPGInput')
+    decoded_image = tf.image.decode_jpeg(jpeg_data, channels=3)
+    # Convert from full range of uint8 to range [0,1] of float32.
+    decoded_image_as_float = tf.image.convert_image_dtype(decoded_image, tf.float32)
+    decoded_image_4d = tf.expand_dims(decoded_image_as_float, 0)
+    resize_shape = tf.stack([height, width])
+    resize_shape_as_int = tf.cast(resize_shape, dtype=tf.int32)
+    resized_image = tf.image.resize_bilinear(decoded_image_4d, resize_shape_as_int)
+    return jpeg_data, resized_image
+
+
+def main():
+    args = docopt(__doc__, help=True)
+    image_dir = args['<image_dir>']
+    output_prefix = args['<output_prefix>']
+    image_paths = glob.glob(image_dir + '/*.jpg')
+    if len(image_path) == 0:
+        print('no images (or image directory) found.', file=sys.stderr)
+        sys.exit(1)
+
+    module_spec = hub.load_module_spec(MODULE_URL)
+    graph, bottleneck_tensor, resized_image_tensor = create_module_graph(module_spec)
+
+    with tf.Session(graph=graph) as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        jpeg_data_tensor, decoded_image_tensor = add_jpeg_decoding(module_spec)
+
+        output_dir = os.path.join(output_prefix, 'feature_vectors', MODULE_NAME)
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+
+        for img_path in image_paths:
+            fn = os.path.basename(img_path)
+            assert fn.endswith('.jpg')
+            fn = fn[:-4] + '.npy'
+            output_path = os.path.join(output_dir, fn)
+            if not os.path.exists(output_path):
+                image_data = open(img_path, 'rb').read()
+                bottleneck_values = run_bottleneck_on_image(sess, jpeg_data_tensor, decoded_image_tensor,
+                                                            resized_image_tensor, bottleneck_tensor)
+                np.save(output_path, bottleneck_values)
